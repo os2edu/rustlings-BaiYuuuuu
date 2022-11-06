23cf6442a10c72d2a7b0b72684f91ac671b05fea
BaiYuuuuu <baiyu21bme@mail.ustc.edu.cn>
2022-11-07 05:51:08 +0800

    Finished the threads part

--- exercises/standard_library_types/cow1.rs
+++ exercises/standard_library_types/cow1.rs
@@ -40,8 +40,7 @@ fn main() {
     let mut input = Cow::from(slice);
     match abs_all(&mut input) {
         // TODO
-        Cow::Owned(_) => println!("not clone occurs"),
-        Cow::Borrowed(_) => println!("I own this slice!"),
+        Cow::Owned(_) => println!("I own this slice!"),
         _ => panic!("expected borrowed value"),
     }
 }
